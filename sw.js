2683102d38d86787c729aeb476bbb9e9eccef2c6
tattooguy518 <dambrositony@gmail.com>
2025-11-18 18:04:27 -0500

    Create a basic Service Worker in sw.js
    Add a basic Service Worker with fetch event listener

---
+++ sw.js
@@ -0,0 +1,4 @@
+self.addEventListener('fetch', function(event) { 
+    // This is the minimum Service Worker required for installation.
+    // We would add caching logic here later for offline capabilities.
+});
